c6001a13cff42809a658649761239b4a9806c266
ZathSL <simone.lissandrello@edu.unito.it>
2022-04-08 01:19:20 +0200

    Cambiato il data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (id, username, nickname, email, password, role, creator_id, enabled, locked)
 VALUES (0, 'test00001', 'test user', 'test@example.com', 'password', 'USER', NULL, true, false);
-INSERT INTO creator (id, name, surname, birth_date, bio, creator_type, avatar)
+INSERT INTO creator (id, name, surname, birth_date, bio, creator_type, avatar, payment_email)
 VALUES (0, 'Creator', 'Test', to_date('1985-05-16', 'YYYY-MM-DD'), 'Biography', 'ARTIST',
 		'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=','swreghe97@gmail.com');
 INSERT INTO users (id, username, nickname, email, password, role, creator_id, enabled, locked)
