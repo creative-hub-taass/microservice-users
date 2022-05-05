06370f5dfebab7baed68cc35aae485bd72be02be
Alessio <51161852+alessiofamiani@users.noreply.github.com>
2022-05-05 18:07:19 +0200

    Inserted CrossOrigin on UserController

--- src/main/java/com/creativehub/backend/controllers/UserController.java
+++ src/main/java/com/creativehub/backend/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
